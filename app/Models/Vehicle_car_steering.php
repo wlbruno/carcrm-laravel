159782bb5488de13360071b84a221bbf02872fb6
wlbruno <willianbruno.sespe@gmail.com>
2021-03-06 17:07:27 -0300

    data olx

---
+++ app/Models/Vehicle_car_steering.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle_car_steering extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DataScraping;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'store']);
+
+//Route::get('/olx/{id}', [DataScraping::class, 'index']);
